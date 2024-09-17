d1d8657cc9623512963c201f281243b2d2c1a87d
Esraaelsisy <esraa.elsisy@gmail.com>
2024-09-17 16:51:58 +0200

    Adding pets tests for Pets Funcalities in petsTests.spec.ts file

---
+++ e2e/ui/specs/petsTests.spec.ts
@@ -0,0 +1,17 @@
+import {  test , expect} from "../fixtures/my-hooks"
+import { PageManager } from "../pages/pageManager"
+import {PetTestData} from "../data/uiTestData/uiTestData.json"
+
+let pm: PageManager
+
+test.describe('Add Pets Tests', async()=>{
+  test('add a new Pet', async({page, ownersFixture})=>{
+    const pm = new PageManager(page)
+    await pm.onOwnersPage().selectAnOwnerFromListWithCriteria(PetTestData.ownerName)
+    await pm.onOwnerOverviewPage().gotoaddNewPetPage()
+    await pm.onPetsPage().addNewPetDetails(PetTestData.name, PetTestData.type, PetTestData.birthdate)
+    await expect(pm.onOwnerOverviewPage().validatePetDetailsRaw(PetTestData.ownerName,PetTestData.name, 
+      PetTestData.type, PetTestData.birthdate))
+})
+})
+
