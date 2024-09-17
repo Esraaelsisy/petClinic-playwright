a25a831f74d27bec2fdc9d6b958bd9723ede50a0
Esraaelsisy <esraa.elsisy@gmail.com>
2024-09-17 16:53:41 +0200

    Adding petVisits Test in visitsTests.spec.ts file

---
+++ e2e/ui/specs/visitsTests.spec.ts
@@ -0,0 +1,17 @@
+import { test , expect} from "../fixtures/my-hooks"
+import { PageManager } from "../pages/pageManager"
+import {VisitsTestData} from "../data/uiTestData/uiTestData.json"
+
+let pm: PageManager
+
+test.describe('Add Pet Visits Tests', async()=> {
+  test('add Pet Visit with valid data', async({page , ownersFixture})=>{
+  const pm = new PageManager(page)
+  await pm.onOwnersPage().selectAnOwnerFromListWithCriteria('George')
+  await pm.onOwnerOverviewPage().gotoVisitsPage()
+  await pm.onVisitsPage().addNewVisitDetails('new visit for today')
+  await pm.onOwnerOverviewPage().gotoVisitsPage()
+//  await expect(pm.onVisitsPage().
+})
+})
+
