73e094891b11a77a038c311e3453e0128b6401e8
Esraaelsisy <esraa.elsisy@gmail.com>
2024-09-18 17:09:37 +0200

    Adding Pets tests in petsTests.spec.ts file

--- e2e/ui/specs/petsTests.spec.ts
+++ e2e/ui/specs/petsTests.spec.ts
@@ -1,17 +1,72 @@
-import {  test , expect} from "../fixtures/my-hooks"
+import { test, expect } from "../fixtures/my-hooks"
 import { PageManager } from "../pages/pageManager"
-import {PetTestData} from "../data/uiTestData.json"
+import { PetTestData } from "../data/uiTestData.json"
 
 let pm: PageManager
 
-test.describe('Add Pets Tests', async()=>{
-  test('add a new Pet', async({page, ownersFixture})=>{
+// Describes the test suite for adding new pets
+test.describe('Add Pets Tests', async () => {
+
+  // Test to add a new pet for an existing owner
+  test('add a new Pet', async ({ page, ownersFixture }) => {
     const pm = new PageManager(page)
-    await pm.onOwnersPage().selectAnOwnerFromListWithCriteria(PetTestData.ownerName)
-    await pm.onOwnerOverviewPage().gotoaddNewPetPage()
-    await pm.onPetsPage().addNewPetDetails(PetTestData.name, PetTestData.type, PetTestData.birthdate)
-    await expect(pm.onOwnerOverviewPage().validatePetDetailsRaw(PetTestData.ownerName,PetTestData.name, 
-      PetTestData.type, PetTestData.birthdate))
+
+    await test.step('Select an owner from the list based on criteria', async () => {
+      // Select the owner from the list using the owner name from PetTestData
+      await pm.onOwnersPage().selectAnOwnerFromListWithCriteria(PetTestData.ownerName)
+    })
+
+    await test.step('Navigate to the Add New Pet page', async () => {
+      // Navigate to the Add New Pet page
+      await pm.onOwnerOverviewPage().gotoAddNewPetPage()
+    })
+
+    await test.step('Add new pet details', async () => {
+      // Add new pet details using PetTestData (name, type, birthdate)
+      await pm.onPetsPage().addNewPetDetails(PetTestData.ownerName,PetTestData.name, PetTestData.type, pm.getTodayDateinCertianFormat())
+    })
+
+    await test.step('Validate the pet details after addition', async () => {
+      // Validate the pet details on the Owner Overview page
+      await expect(pm.onOwnerOverviewPage().validatePetDetailsRow(
+        PetTestData.ownerName,
+        PetTestData.name,
+        PetTestData.type
+      ))
+    })
+  })
 })
+
+// Describes the test suite for editing new pets
+test.describe('Edit Pets Tests', async () => {
+
+  // Test to add a new pet for an existing owner
+  test('edit a Pet', async ({ page, ownersFixture }) => {
+    const pm = new PageManager(page)
+
+    await test.step('Select an owner from the list based on criteria', async () => {
+      // Select the owner from the list using the owner name from PetTestData
+      await pm.onOwnersPage().selectAnOwnerFromListWithCriteria(PetTestData.ownerName)
+    })
+
+    await test.step('Navigate to the Add New Pet page', async () => {
+      // Navigate to the Add New Pet page
+      await pm.onOwnerOverviewPage().gotoEditNewPetPage()
+    })
+
+    await test.step('Add new pet details', async () => {
+      // Add new pet details using PetTestData (name, type, birthdate)
+      await pm.onPetsPage().addNewPetDetails(PetTestData.ownerName,PetTestData.name, PetTestData.type, pm.getTodayDateinCertianFormat())
+    })
+
+    await test.step('Validate the pet details after addition', async () => {
+      // Validate the pet details on the Owner Overview page
+      await expect(pm.onOwnerOverviewPage().validatePetDetailsRow(
+        PetTestData.ownerName,
+        PetTestData.name,
+        PetTestData.type
+      ))
+    })
+  })
 })
 
