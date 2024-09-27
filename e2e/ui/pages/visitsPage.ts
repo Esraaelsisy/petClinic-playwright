ae479e70e52f9559f70b2e4bc8f4bb1e27815b1e
Esraaelsisy <esraa.elsisy@gmail.com>
2024-09-28 01:42:40 +0200

    Fix adding visits UI flaky tests

--- e2e/ui/pages/visitsPage.ts
+++ e2e/ui/pages/visitsPage.ts
@@ -5,6 +5,7 @@ export class VisitsPage{
     readonly page: Page
     readonly header: HeaderPage
     readonly descriptionInput: Locator
+    readonly dateInput: Locator
     readonly addNewVisitButton: Locator
     readonly visitDetailsRow: Locator
     readonly visitDescriptionText: Locator
@@ -14,16 +15,19 @@ export class VisitsPage{
         this.page = page
         this.header =  new HeaderPage(this.page)
         this.descriptionInput = page.locator('textarea[ng-model="$ctrl.desc"]')
+        this.dateInput = page.locator('input[type="date"]')
         this.addNewVisitButton = page.getByRole('button').filter({hasText: "Add New Visit"})
+        this.visitDescriptionText = page.getByRole("table").getByRole("row").first().getByRole("cell").nth(1)
+        this.visitDateText = page.getByRole("table").getByRole("row").first().getByRole("cell").first()
     }
 
-    async addNewVisitDetails(description: string){
+    async addNewVisitDetails(description: string, date: string){
         await this.descriptionInput.fill(description)
+        await this.dateInput.pressSequentially(date)
         await this.addNewVisitButton.click()
     }
 
-    async validateVisitDetailsRow(visitDescripton: string, visitDate: string){
+    async validateVisitDetailsRow(visitDescripton: string){
         await expect(this.visitDescriptionText).toContainText(visitDescripton)
-        await expect(this.visitDateText).toContainText(visitDate)
     }
 }
--- e2e/ui/specs/visitsTests.spec.ts
+++ e2e/ui/specs/visitsTests.spec.ts
@@ -5,7 +5,7 @@ import { VisitsTestData } from "../data/uiTestData.json"
 let pm: PageManager
 
 // Describes the test suite for adding pet visits
-test.describe('Add Pet Visits Tests', async () => {
+test.describe('Add Pet Visits Tests @happy', async () => {
 
   // Test to add a new visit for a pet with valid data
   test('add Pet Visit with valid data', async ({ page, ownersFixture }) => {
@@ -18,20 +18,20 @@ test.describe('Add Pet Visits Tests', async () => {
 
     await test.step('Navigate to the Visits page', async () => {
       // Navigate to the Visits page
-      await pm.onOwnerOverviewPage().gotoVisitsPage()
+      await pm.onOwnerOverviewPage().gotoVisitsPageWithPetName(VisitsTestData.petName)
     })
 
     await test.step('Add new visit details for the pet', async () => {
       // Add new visit details using a description from VisitsTestData
-      await pm.onVisitsPage().addNewVisitDetails('new visit for today')
+      await pm.onVisitsPage().addNewVisitDetails(VisitsTestData.description, VisitsTestData.date)
     })
 
     await test.step('Verify the visit details by navigating to the Visits page', async () => {
       // Navigate back to the Visits page to verify the new visit was added correctly
-      await pm.onOwnerOverviewPage().gotoVisitsPage()
+      await pm.onOwnerOverviewPage().gotoVisitsPageWithPetName(VisitsTestData.petName)
 
       // Add the appropriate expectation here to validate the visit details
-     // await expect(pm.onVisitsPage().validateVisitDetailsRow(VisitsTestData.description , VisitsTestData.date))
+     await pm.onVisitsPage().validateVisitDetailsRow(VisitsTestData.description)
     })
   })
 })
