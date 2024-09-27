d3552fae34873e9db895051df3938562593da62e
Esraaelsisy <esraa.elsisy@gmail.com>
2024-09-27 16:03:53 +0200

    Updating config file to include the api tests as a project

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './e2e', // Define test directory to point to e2e for both UI and API
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -10,21 +10,32 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:8080',
     trace: 'on-first-retry',
-    // Toggle headless mode with environment variable
-    headless: process.env.HEADLESS !== 'false',
+    headless: process.env.HEADLESS !== 'false', // Toggle headless mode via environment variable
   },
   projects: [
+    // UI Project
     {
-      name: 'chromium',
+      name: 'ui-chromium',
+      testDir: 'e2e/ui/specs', // Point to your UI tests
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'firefox',
+      name: 'ui-firefox',
+      testDir: 'e2e/ui/specs', // Point to your UI tests
       use: { ...devices['Desktop Firefox'] },
     },
     {
-      name: 'webkit',
+      name: 'ui-webkit',
+      testDir: 'e2e/ui/specs', // Point to your UI tests
       use: { ...devices['Desktop Safari'] },
+    },
+    // API Project
+    {
+      name: 'api',
+      testDir: 'e2e/api/tests',  // Point to your API tests directory
+      use: {
+        headless: true, // API tests don't require a browser UI
+      },
     },
   ],
   /*webServer: {
